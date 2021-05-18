0607c8b1b8cb0395a7d00a8e9a349fd7527a3a35
daliayan <daliayan5@gmail.com>
2021-05-18 15:10:48 -0400

    RESTful actions for index, show, new, create, edit & update - USING GENERATORS, nusing form_for - redirect_to, linking between pages, form creation, routes, database creating, - all using GENERATORS

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
-  resources :genres, only: [:index, :show, :new, :create, :edit, :update]
-  resources :artists, only: [:index, :show, :new, :create, :edit, :update]
+  resources :songs
+  resources :genres
+  resources :artists
 end
